6f79e53378f3a9805f03d10f23561fe90c771543
langat justus <justuslangat78@gmail.com>
2020-11-01 09:30:40 +0300

    added a navbar

--- nyumbakumi/urls.py
+++ nyumbakumi/urls.py
@@ -7,5 +7,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('my-profile/', views.my_profile, name='my-profile'),
     
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- nyumbakumi/views.py
+++ nyumbakumi/views.py
@@ -11,3 +11,18 @@ def index(request):
         return redirect('create-profile')
 
     return render(request,'index.html')
+
+@login_required(login_url='/accounts/login/')
+def my_profile(request):
+    current_user=request.user
+    profile =Profile.objects.get(username=current_user)
+
+    return render(request,'user_profile.html',{"profile":profile})
+
+
+@login_required(login_url='/accounts/login/')
+def user_profile(request,username):
+    user = User.objects.get(username=username)
+    profile =Profile.objects.get(username=user)
+
+    return render(request,'user_profile.html',{"profile":profile})
