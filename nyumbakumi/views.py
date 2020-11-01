4e7cd37848c0af1de01530b11a5a5f9afdb3f41d
langat justus <justuslangat78@gmail.com>
2020-11-01 11:52:31 +0300

    fixed bug

--- nyumbakumi/views.py
+++ nyumbakumi/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import ProfileForm
-from .models import Profile, healthservices
+from .models import Profile, healthservices, Authorities, Health
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse,Http404,HttpResponseRedirect
 
